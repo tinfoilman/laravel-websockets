ea9605923ecaa3fa2caa9d57e675ce5ef3a80fc3
tinfoilman <56478511+tinfoilman@users.noreply.github.com>
2020-07-23 23:00:08 -0400

    Update websockets.php

--- config/websockets.php
+++ config/websockets.php
@@ -90,7 +90,7 @@ return [
          * When the clean-command is executed, all recorded statistics older than
          * the number of days specified here will be deleted.
          */
-        'delete_statistics_older_than_days' => 60,
+        'delete_statistics_older_than_days' => 61,
 
         /*
          * Use an DNS resolver to make the requests to the statistics logger
